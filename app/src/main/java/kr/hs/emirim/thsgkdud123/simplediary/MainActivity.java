f877f00063e34b543296702e08eabcd0130b9104
HaYoung Son <thsgkdud123@naver.com>
2016-10-18 19:42:33 +0900

    #4 implements to butSave EventHandler

--- app/src/main/java/kr/hs/emirim/thsgkdud123/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/thsgkdud123/simplediary/MainActivity.java
@@ -1,13 +1,17 @@
 package kr.hs.emirim.thsgkdud123.simplediary;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -38,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
             String content=readDiary(fileName);
                 editDiary.setText(content);
                 butSave.setEnabled(true);
+            }
+        });
+        butSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream out=openFileOutput(fileName, Context.MODE_WORLD_WRITEABLE);
+                    String diaryContents=editDiary.getText().toString();
+                    out.write(diaryContents.getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(),"저장이 완료됨", Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
